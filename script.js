e4852c5a6f6ea5acbc2644c136cfa94f4ae9fdd2
SIDDHARTH SINGH <100992573+SiddharthSingh022@users.noreply.github.com>
2023-06-04 11:16:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,16 +7,16 @@ let gif = document.getElementById('gif');
 let songItem = Array.from(document.getElementsByClassName('songItem'));
 let songs = [
   { songName: "Tujhe Sochta hoon", filePath: "songs/1.mp3", coverPath: "1.jpg" },
-  { songName: "Kya Mujhe Pyar Hai", filePath: "songs/2.mp3", coverPath: "covers/2.jpg" },
-  { songName: "Masakali", filePath: "songs/3.mp3", coverPath: "covers/3.jpg" },
-  { songName: "Dil Ibaadat", filePath: "songs/4.mp3", coverPath: "covers/4.jpg" },
-  { songName: "Tum Ho", filePath: "songs/5.mp3", coverPath: "covers/5.jpg" },
-  { songName: "Tujhe Bula Diya", filePath: "songs/6.mp3", coverPath: "covers/6.jpg" },
-  { songName: "Dooriya", filePath: "songs/7.mp3", coverPath: "covers/7.jpg" },
-  { songName: "Tu Hi Haqeeqat", filePath: "songs/8.mp3", coverPath: "covers/8.jpg" },
-  { songName: "Kudha Jaane", filePath: "songs/9.mp3", coverPath: "covers/9.jpg" },
-  { songName: "Zara Sa", filePath: "songs/10.mp3", coverPath: "covers/10.jpg" },
-  { songName: "Tu Hi Meri Shab Hai", filePath: "songs/11.mp3", coverPath: "covers/11.jpg" },
+  { songName: "Kya Mujhe Pyar Hai", filePath: "songs/2.mp3", coverPath: "2.jpg" },
+  { songName: "Masakali", filePath: "songs/3.mp3", coverPath: "3.jpg" },
+  { songName: "Dil Ibaadat", filePath: "songs/4.mp3", coverPath: "1.jpg" },
+  { songName: "Tum Ho", filePath: "songs/5.mp3", coverPath: "5.jpg" },
+  { songName: "Tujhe Bula Diya", filePath: "songs/6.mp3", coverPath: "6.jpg" },
+  { songName: "Dooriya", filePath: "songs/7.mp3", coverPath: "7.jpg" },
+  { songName: "Tu Hi Haqeeqat", filePath: "songs/8.mp3", coverPath: "8.jpg" },
+  { songName: "Kudha Jaane", filePath: "songs/9.mp3", coverPath: "9.jpg" },
+  { songName: "Zara Sa", filePath: "songs/10.mp3", coverPath: "10.jpg" },
+  { songName: "Tu Hi Meri Shab Hai", filePath: "songs/11.mp3", coverPath: "1.jpg" },
 ];
 
 $(document).ready(function() {
